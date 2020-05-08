651fd88148ed6de8936c5823d9ba1a3f30e28c76
Kacper Szot <kacperszot1@gmail.com>
2020-05-08 19:08:56 +0200

    add tests for advertisement builder

--- src/test/java/szot/pl/simplejobboard/model/AdvertisementTest.java
+++ src/test/java/szot/pl/simplejobboard/model/AdvertisementTest.java
@@ -47,5 +47,40 @@ class AdvertisementTest {
         Assertions.assertEquals(advertisement.getExpirationDate(), EXPIRATION_DATE, "advertisement should contain expiration date field");
     }
 
+    @Test
+    public void advertisementBuilderShouldCreateAnObjectWithTheValidTitleField() {
+        final String TITLE = "test title";
+        Advertisement advertisement = new Advertisement.Builder().title(TITLE).build();
+        Assertions.assertEquals(advertisement.getTitle(), TITLE, "advertisement builder should create an object with the valid title field");
+    }
+
+    @Test
+    public void advertisementBuilderShouldCreateAnObjectWithTheValidContentField() {
+        final String CONTENT = "test content";
+        Advertisement advertisement = new Advertisement.Builder().content(CONTENT).build();
+        Assertions.assertEquals(advertisement.getContent(), CONTENT, "advertisement builder should create an object with the valid content field");
+    }
+
+    @Test
+    public void advertisementBuilderShouldCreateAnObjectWithTheValidIdField() {
+        final Long ID = 0L;
+        Advertisement advertisement = new Advertisement.Builder().id(ID).build();
+        Assertions.assertEquals(advertisement.getId(), ID, "advertisement builder should create an object with the valid id field");
+    }
+
+    @Test
+    public void advertisementBuilderShouldCreateAnObjectWithTheValidCreationDateField() {
+        final LocalDateTime CREATION_DATE = LocalDateTime.of(2020, 5, 8, 18, 0);
+        Advertisement advertisement = new Advertisement.Builder().creationDate(CREATION_DATE).build();
+        Assertions.assertEquals(advertisement.getCreationDate(), CREATION_DATE, "advertisement builder should create a object with valid creation date field");
+    }
+
+    @Test
+    public void advertisementBuilderShouldCreateAnObjectWithTheValidExpirationDateField() {
+        final LocalDateTime EXPIRATION_DATE = LocalDateTime.of(2020, 5, 8, 18, 0);
+        Advertisement advertisement = new Advertisement.Builder().expirationDate(EXPIRATION_DATE).build();
+        Assertions.assertEquals(advertisement.getExpirationDate(), EXPIRATION_DATE, "advertisement builder should create a object with valid expiration date field");
+    }
+
 }
 
