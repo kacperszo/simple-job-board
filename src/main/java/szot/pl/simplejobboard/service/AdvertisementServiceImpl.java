57504bb5eaff9564b586504eb9ebe31225582fa9
Kacper Szot <kacperszot1@gmail.com>
2020-05-09 18:38:25 +0200

    add javaDoc to the AdvertisementServiceImpl

--- src/main/java/szot/pl/simplejobboard/service/AdvertisementServiceImpl.java
+++ src/main/java/szot/pl/simplejobboard/service/AdvertisementServiceImpl.java
@@ -8,6 +8,12 @@ import szot.pl.simplejobboard.repository.AdvertisementRepository;
 
 import java.util.Optional;
 
+/**
+ * implementation of the AdvertisementService
+ *
+ * @author Kacper Szot
+ * @see szot.pl.simplejobboard.service.AdvertisementService
+ */
 @Service
 public class AdvertisementServiceImpl implements AdvertisementService {
 
@@ -17,31 +23,49 @@ public class AdvertisementServiceImpl implements AdvertisementService {
         this.advertisementRepository = advertisementRepository;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Advertisement save(Advertisement advertisement) {
         return advertisementRepository.saveAndFlush(advertisement);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Page<Advertisement> findAll(int pageNumber, int pageSize) {
         return advertisementRepository.findAll(PageRequest.of(pageNumber, pageSize));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Page<Advertisement> search(int pageNumber, int pageSize, String query) {
         return advertisementRepository.findAllByTitleContaining(PageRequest.of(pageNumber, pageSize), query);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Optional<Advertisement> findById(Long id) {
         return advertisementRepository.findById(id);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void delete(Advertisement advertisement) {
         advertisementRepository.delete(advertisement);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void deleteById(Long id) {
         advertisementRepository.deleteById(id);
