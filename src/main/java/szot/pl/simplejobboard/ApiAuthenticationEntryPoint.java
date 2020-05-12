883bd2efa0e2cbe68f61f8bf47651ab23d8f0150
Kacper Szot <kacperszot1@gmail.com>
2020-05-12 17:55:39 +0200

    create custom AuthenticationEntryPoint for api

---
+++ src/main/java/szot/pl/simplejobboard/ApiAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package szot.pl.simplejobboard;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Custom authentication entry point used by api
+ * @author Kacper Szot
+ */
+public class ApiAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
