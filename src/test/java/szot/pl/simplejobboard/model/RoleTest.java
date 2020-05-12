65c3bb83a12f397b7d252668205f23b4e45eb3d4
Kacper Szot <kacperszot1@gmail.com>
2020-05-12 18:46:06 +0200

    add role class

---
+++ src/test/java/szot/pl/simplejobboard/model/RoleTest.java
@@ -0,0 +1,23 @@
+package szot.pl.simplejobboard.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RoleTest {
+
+    @Test
+    void roleShouldShouldContainIdField() {
+        final Long id = 2L;
+        Role role = new Role.Builder().id(id).build();
+        assertEquals(id, role.getId());
+    }
+
+    @Test
+    void roleShouldShouldContainAuthorityField() {
+        final String authority = "test authority";
+        Role role = new Role.Builder().authority(authority).build();
+        assertEquals(authority, role.getAuthority());
+    }
+
+}
