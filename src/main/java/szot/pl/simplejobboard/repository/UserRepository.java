2e1cf8b028af263177a0d9716e8f2610cfc60412
Kacper Szot <kacperszot1@gmail.com>
2020-05-13 19:51:39 +0200

    add find by username to the user repository

---
+++ src/main/java/szot/pl/simplejobboard/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package szot.pl.simplejobboard.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import szot.pl.simplejobboard.model.User;
+
+import java.util.Optional;
+
+/**
+ * User repository
+ *
+ * @author Kacper Szot
+ */
+public interface UserRepository extends CrudRepository<User, Long> {
+    /**
+     * Retrieves an optional of user by its username
+     *
+     * @param username cannot be null
+     * @return Optional of user for given id
+     */
+    Optional<User> findByUsername(String username);
+}
