56a4624974b193b28f9155e1bf0a06b8f984a371
Kacper Szot <kacperszot1@gmail.com>
2020-05-20 19:27:36 +0200

    make api authentication entry point component

--- src/main/java/pl/szot/simplejobboard/security/ApiAuthenticationEntryPoint.java
+++ src/main/java/pl/szot/simplejobboard/security/ApiAuthenticationEntryPoint.java
@@ -2,6 +2,7 @@ package pl.szot.simplejobboard.security;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -10,8 +11,10 @@ import java.io.IOException;
 
 /**
  * Custom authentication entry point used by api
+ *
  * @author Kacper Szot
  */
+@Component
 public class ApiAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
