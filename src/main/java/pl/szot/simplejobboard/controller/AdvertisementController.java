734ca7a21c7fb1773dbadc8333e24c0ebc40314e
Kacper Szot <kacperszot1@gmail.com>
2020-05-21 22:19:19 +0200

    create advertisement controller

---
+++ src/main/java/pl/szot/simplejobboard/controller/AdvertisementController.java
@@ -0,0 +1,94 @@
+package pl.szot.simplejobboard.controller;
+
+import org.springframework.data.domain.Page;
+import org.springframework.security.core.Authentication;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import pl.szot.simplejobboard.dto.AdvertisementDTO;
+import pl.szot.simplejobboard.exceptions.BadRequestException;
+import pl.szot.simplejobboard.exceptions.NotFoundException;
+import pl.szot.simplejobboard.mapper.AdvertisementMapper;
+import pl.szot.simplejobboard.model.Advertisement;
+import pl.szot.simplejobboard.requests.CreateAdvertisementRequest;
+import pl.szot.simplejobboard.requests.UpdateAdvertisementRequest;
+import pl.szot.simplejobboard.service.AdvertisementService;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("v1/advertisements")
+public class AdvertisementController {
+    private AdvertisementService advertisementService;
+    private AdvertisementMapper advertisementMapper;
+
+    public AdvertisementController(AdvertisementService advertisementService, AdvertisementMapper advertisementMapper) {
+        this.advertisementService = advertisementService;
+        this.advertisementMapper = advertisementMapper;
+    }
+
+    @GetMapping("/{id}")
+    public AdvertisementDTO getAdvertisement(@PathVariable Long id, Authentication authentication) {
+        if (isAdminLoggedIn(authentication)) {
+            return advertisementMapper.mapToDto(advertisementService.findById(id).orElseThrow(() -> new NotFoundException("advertisement do not exist")));
+        }
+
+        Optional<Advertisement> advertisement = advertisementService.findById(id);
+
+        if (advertisement.isEmpty() || advertisement.get().isHidden()) {
+            throw new NotFoundException("advertisement do not exist");
+        }
+
+        return advertisementMapper.mapToDto(advertisement.get());
+
+    }
+
+    @GetMapping
+    public Page<AdvertisementDTO> getAdvertisements(
+            Authentication authentication,
+            @RequestParam(defaultValue = "0") Integer pageNo,
+            @RequestParam(defaultValue = Integer.MAX_VALUE + "") Integer pageSize
+    ) {
+        if (isAdminLoggedIn(authentication)) {
+            return advertisementService.findAll(pageNo, pageSize).map(s -> advertisementMapper.mapToDto(s));
+        }
+        return advertisementService.findAllNotExpiredAndNotHidden(pageNo, pageSize).map(s -> advertisementMapper.mapToDto(s));
+    }
+
+    @PostMapping("/{id}")
+    public AdvertisementDTO update(@PathVariable Long id, @Valid @RequestBody UpdateAdvertisementRequest request, BindingResult result) {
+        if (result.hasErrors()) {
+            throw new BadRequestException(result);
+        }
+        Optional<Advertisement> advertisementOptional = advertisementService.findById(id);
+        if (advertisementOptional.isEmpty()) {
+            throw new NotFoundException("advertisement do not exist");
+        }
+        return advertisementMapper.mapToDto(advertisementService.save(advertisementOptional.get()));
+    }
+
+    @PostMapping()
+    public AdvertisementDTO create(@Valid @RequestBody CreateAdvertisementRequest request, BindingResult result) {
+        if (result.hasErrors()) {
+            throw new BadRequestException(result);
+        }
+        Advertisement advertisement = new Advertisement.Builder()
+                .title(request.getTitle())
+                .content(request.getContent())
+                .expirationDate(request.getExpirationDate())
+                .hidden(request.getHidden())
+                .build();
+        return advertisementMapper.mapToDto(advertisementService.save(advertisement));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        advertisementService.deleteById(id);
+    }
+
+
+    private boolean isAdminLoggedIn(Authentication authentication) {
+        return authentication != null;
+    }
+}
---
+++ src/test/java/pl/szot/simplejobboard/controller/AdvertisementControllerTest.java
@@ -0,0 +1,151 @@
+package pl.szot.simplejobboard.controller;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import pl.szot.simplejobboard.model.Advertisement;
+import pl.szot.simplejobboard.service.AdvertisementService;
+import pl.szot.simplejobboard.service.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AdvertisementControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+    @MockBean
+    AdvertisementService advertisementService;
+    @MockBean
+    UserService userService;
+
+    @Test
+    void getAdvertisementShouldReturn200AnonymousAccessScenario() throws Exception {
+        Advertisement advertisement = new Advertisement.Builder().id(2L).hidden(false).title("title").content("content").build();
+        Mockito.when(advertisementService.findById(Mockito.any())).thenReturn(java.util.Optional.ofNullable(advertisement));
+        mockMvc.perform(get("/v1/advertisements/2")).andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "admin")
+    void getAdvertisementShouldReturn200AdminAccessScenario() throws Exception {
+        Advertisement advertisement = new Advertisement.Builder().id(2L).hidden(true).title("title").content("content").build();
+        Mockito.when(advertisementService.findById(Mockito.any())).thenReturn(java.util.Optional.ofNullable(advertisement));
+        mockMvc.perform(get("/v1/advertisements/2")).andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    void getAdvertisementShouldReturn404AnonymousAccessScenario() throws Exception {
+        Advertisement advertisement = new Advertisement.Builder().id(2L).hidden(true).title("title").content("content").build();
+        Mockito.when(advertisementService.findById(Mockito.any())).thenReturn(java.util.Optional.ofNullable(advertisement));
+        mockMvc.perform(get("/v1/advertisements/2")).andDo(print()).andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithMockUser(username = "admin")
+    void getAdvertisementShouldReturn404AdminAccessScenario() throws Exception {
+        Mockito.when(advertisementService.findById(Mockito.any())).thenReturn(Optional.empty());
+        mockMvc.perform(get("/v1/advertisements/2")).andDo(print()).andExpect(status().isNotFound());
+    }
+
+    @Test
+    void getAdvertisementsShouldReturn200AnonymousAccessScenario() throws Exception {
+        List<Advertisement> advertisements = new ArrayList<>();
+        advertisements.add(new Advertisement.Builder().id(2L).hidden(true).title("title").content("content").build());
+        Mockito.when(advertisementService.findAllNotExpiredAndNotHidden(0, 100)).thenReturn(new PageImpl<Advertisement>(advertisements));
+        mockMvc.perform(get("/v1/advertisements?pageNo=0&pageSize=100"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .string(containsString("content")));
+    }
+
+    @Test
+    @WithMockUser(username = "admin")
+    void getAdvertisementsShouldReturn200AdminAccessScenario() throws Exception {
+        List<Advertisement> advertisements = new ArrayList<>();
+        advertisements.add(new Advertisement.Builder().id(2L).hidden(true).title("title").content("content").build());
+        Mockito.when(advertisementService.findAll(0, 100)).thenReturn(new PageImpl<Advertisement>(advertisements));
+        mockMvc.perform(get("/v1/advertisements?pageNo=0&pageSize=100"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .string(containsString("content")));
+    }
+
+    @Test
+    @WithMockUser(username = "admin")
+    void updateShouldReturn200() throws Exception {
+        Advertisement advertisement = new Advertisement.Builder().id(2L).hidden(true).title("title").content("content").build();
+        Mockito.when(advertisementService.findById(Mockito.any())).thenReturn(java.util.Optional.ofNullable(advertisement));
+        Mockito.when(advertisementService.save(Mockito.any())).thenReturn(advertisement);
+        mockMvc.perform(post("/v1/advertisements/2").contentType(MediaType.APPLICATION_JSON)
+                .content("{\"title\":\"admin\",\"content\":\"admin\",\"expirationDate\":\"2023-02-06T03:45:42.01\",\"hidden\":true}"))
+                .andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "admin")
+    void updateShouldReturn401() throws Exception {
+        Advertisement advertisement = new Advertisement.Builder().id(2L).hidden(true).title("title").content("content").build();
+        Mockito.when(advertisementService.findById(Mockito.any())).thenReturn(java.util.Optional.ofNullable(advertisement));
+        Mockito.when(advertisementService.save(Mockito.any())).thenReturn(advertisement);
+        mockMvc.perform(post("/v1/advertisements/2").contentType(MediaType.APPLICATION_JSON)
+                .content("{\"title\":\"admin\"onDate\":\"2023-02-06T03:45:42.01\",\"hidden\":true}"))
+                .andDo(print()).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @WithMockUser(username = "admin")
+    void updateShouldReturn404() throws Exception {
+        Advertisement advertisement = new Advertisement.Builder().id(2L).hidden(true).title("title").content("content").build();
+        Mockito.when(advertisementService.findById(Mockito.any())).thenReturn(java.util.Optional.empty());
+        Mockito.when(advertisementService.save(Mockito.any())).thenReturn(advertisement);
+        mockMvc.perform(post("/v1/advertisements/2").contentType(MediaType.APPLICATION_JSON)
+                .content("{\"title\":\"admin\",\"content\":\"admin\",\"expirationDate\":\"2023-02-06T03:45:42.01\",\"hidden\":true}"))
+                .andDo(print()).andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithMockUser(username = "admin")
+    void createShouldReturn200() throws Exception {
+        Advertisement advertisement = new Advertisement.Builder().id(2L).hidden(true).title("title").content("content").build();
+        Mockito.when(advertisementService.save(Mockito.any())).thenReturn(advertisement);
+        mockMvc.perform(post("/v1/advertisements").contentType(MediaType.APPLICATION_JSON)
+                .content("{\"title\":\"admin\",\"content\":\"admin\",\"expirationDate\":\"2023-02-06T03:45:42.01\",\"hidden\":true}"))
+                .andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "admin")
+    void createShouldReturn401() throws Exception {
+        Advertisement advertisement = new Advertisement.Builder().id(2L).hidden(true).title("title").content("content").build();
+        Mockito.when(advertisementService.save(Mockito.any())).thenReturn(advertisement);
+        mockMvc.perform(post("/v1/advertisements").contentType(MediaType.APPLICATION_JSON)
+                .content("{\"title\":\"admin\",\"conteate\":\"2023-02-06T03:45:42.01\",\"hidden\":true}"))
+                .andDo(print()).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @WithMockUser(username = "admin")
+    void deleteShouldReturn200() throws Exception {
+        mockMvc.perform(delete("/v1/advertisements/2"))
+                .andDo(print()).andExpect(status().isOk());
+    }
+}
